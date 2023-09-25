f269667a7e759bd22277c1c04e6194741fb69b8f
mmishv <pinina7773@gmail.com>
2023-09-25 15:43:05 +0300

    refactor auth

--- src/app/core/interceptor/token.interceptor.ts
+++ src/app/core/interceptor/token.interceptor.ts
@@ -1,9 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
+  HttpInterceptor, HttpRequest, HttpHandler, HttpEvent,
 } from '@angular/common/http';
 import {catchError, Observable, switchMap} from 'rxjs';
 import {AuthService} from "../service/auth.service";
@@ -11,56 +8,54 @@ import {Router} from "@angular/router";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService, private router: Router) {}
+  private refreshAttempts = 0;
+  private readonly maxRefreshAttempts = 2;
 
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const url = request.url.toLowerCase();
-    if (url.includes('/auth/login') || url.includes('/auth/signup')) {
+    if (url.includes('/auth/login') || url.includes('reset-password')) {
       return next.handle(request);
     }
     const token = this.authService.getToken();
     if (token) {
       request = this.addTokenToRequest(request, token);
     }
-    return next.handle(request).pipe(
-      catchError((error) => {
-        if (error.status === 401) {
+    return next.handle(request).pipe(catchError((error) => {
+      if ([401, 403].includes(error.status)) {
+        if (this.refreshAttempts < this.maxRefreshAttempts) {
+          this.refreshAttempts++;
           return this.refreshTokenAndRetry(request, next);
         } else {
-          throw error;
+          this.authService.logout();
+          return new Observable<HttpEvent<any>>();
         }
-      })
-    );
+      } else {
+        throw error;
+      }
+    }));
   }
 
   private addTokenToRequest(request: HttpRequest<any>, token: string): HttpRequest<any> {
     return request.clone({
-        setHeaders: {
-          token: `${token}`,
-          HTTP_TOKEN: `${token}`,
-        },
+      setHeaders: {
+        token: `${token}`, HTTP_TOKEN: `${token}`,
+      },
     });
   }
 
-  private refreshTokenAndRetry(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    return this.authService.refreshTokens().pipe(
-      switchMap((response) => {
-        if (response.access_token) {
-          const newToken = response.access_token;
-          request = this.addTokenToRequest(request, newToken);
-          return next.handle(request);
-        } else {
-          this.authService.logout();
-          this.router.navigate(['/auth/login']);
-          return new Observable<HttpEvent<any>>();
-        }
-      })
-    );
+  private refreshTokenAndRetry(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.authService.refreshTokens().pipe(switchMap((response) => {
+      if (response.access_token) {
+        const newToken = response.access_token;
+        request = this.addTokenToRequest(request, newToken);
+        return next.handle(request);
+      } else {
+        this.authService.logout();
+        return new Observable<HttpEvent<any>>();
+      }
+    }));
   }
 }
